60af80094bf4515a18fcfd41950a2e02609b0b40
Ubaydullo Asatullaev <Ubaydullohazard@gmail.com>
2024-12-16 13:30:18 +0900

    Update Array Transformation with Multiplicative Operations.py

--- 2024/Array Transformation with Multiplicative Operations.py
+++ 2024/Array Transformation with Multiplicative Operations.py
@@ -1,1 +1,19 @@
+# 3264. Final Array State After K Multiplication Operations I
 
+class Solution(object):
+  def getFinalState(self, nums, k, multiplier):
+            """
+        :type nums: List[int]
+        :type k: int
+        :type multiplier: int
+        :rtype: List[int]
+        """
+      for _ in range(k):
+        # find minimum value and its index
+        minValue = min(nums)
+        minIndex = nums.index(minValue)
+
+        # replace min value with new values
+        nums[minIndex] = minValue * multiplier
+      return nums
+        
